11e2a9388d42c55e4fb65bed2d3ed390c2dc9740
greenigot7 <greenigot7@gmail.com>
2019-05-07 17:53:16 +0700

    Initial commit

---
+++ Server.java
@@ -0,0 +1,51 @@
+//package Server;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.lang.ClassNotFoundException;
+import java.net.Socket;
+import java.util.Random;
+
+public class Server {
+    //socket server port on which it will listen
+    private static int PORT = 8080;
+    //static ServerSocket variable
+    private static ServerSocket server;
+
+    public static void main(String[] args) throws ClassNotFoundException {
+        int i =0;
+        Thread thread = null;
+        try {
+            //Create ServerSocket
+            server = new ServerSocket(PORT);
+            while (true) {
+                System.out.println("Waiting for the client request");
+                Socket socket = server.accept();
+                ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+
+                Random rand = new Random();
+                //Generate Thread and port
+                int newPort = rand.nextInt(9000)+1000;
+                MultiThreadRespond multiThreadRespond = new MultiThreadRespond(newPort);
+                thread = new Thread(multiThreadRespond);
+                thread.start();
+                ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+                oos.writeObject(""+newPort);
+
+                ois.close();
+                oos.close();
+                socket.close();
+                i++;
+            }
+        }
+        catch (IOException ex) {
+           try {
+                server.close();
+            } catch (IOException e) {
+                System.err.println("ERROR closing socket: " + e.getMessage());
+            }
+        }
+    }
+}
